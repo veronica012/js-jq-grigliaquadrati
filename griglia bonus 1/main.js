2652b14778d443bea14ad7f941a98b9e15745277
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-07 16:02:52 +0200

    cartella con esercizio bonus 1 + style

---
+++ griglia bonus 1/main.js
@@ -0,0 +1,16 @@
+for (var i = 0; i < 25; i++) {
+    $('.griglia').append('<div class="quadrato"></div>');
+};
+$('.quadrato').each(function(){
+    var numero_random = (get_random(0, 10));
+    $(this).append(numero_random);
+    if(numero_random == 0) {
+    $(this).addClass('zero');
+} else if (numero_random  % 2 == 0) {
+    $(this).addClass('pari');
+}
+
+});
+function get_random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
